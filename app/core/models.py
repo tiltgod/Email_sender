30376d7a0b9b927880d35540b8c4e24fc344c0d6
tiltgod <59070084@it.kmitl.ac.th>
2020-12-28 20:42:08 +0700

    Create create_superuser() and modifide create_user for handle invalid email and normalized email

--- app/core/models.py
+++ app/core/models.py
@@ -6,11 +6,23 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
     """For manage user account"""
     def create_user(self, email, password=None, **extra_fields):
-        # take extra augument
-        """create user and save user"""
-        user = self.model(email=email, **extra_fields)
+        """Creates and saves a new User"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        # check valid email
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # normalize_email
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
